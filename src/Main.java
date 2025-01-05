0124bbea3f7f26b952d65347f005c747bbd85130
JBAO <baojonathan95@gmail.com>
2025-01-05 22:06:16 +0100

    Ajout de la vérification de la séquence de chiffres du joueur1 et initialisation des chances du joueur2

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,21 @@ public class Main {
         System.out.println("Bienvenue dans le jeu de MasterMind!");
         System.out.println("Ce jeu est composé de 2 joueurs.");
         System.out.println("Le joueur numéro1 sera celui choisira la séquence de chiffres et le joueur numéro2 devra le deviner.");
-        System.out.println("Choisissez le niveau de diffilculté: ");
-        System.out.println("|Niveau facile=1 | Niveau moyen=2 | Niveau difficile=3 | Niveau extrême=4 |");
-        int niveauDifficulté = clavier.nextInt();
-        System.out.println("Vous avez choisi le niveau "+niveauDifficulté+".");
+        String sequenceChiffres= "";
+
+        System.out.println("Joueur1: Veuillez saisir votre séquence de chiffres composés de 5 CHIFFRES ENTRE 1 ET 9: ");
+        sequenceChiffres = clavier.nextLine();
+
+        while (sequenceChiffres.length() != 5) {
+            System.out.println("Erreur! Veuillez ressaisir 5 CHIFFRES ENTRE 1 ET 9:");
+            sequenceChiffres = clavier.nextLine();
+            }
+
+            System.out.println("Le jeu peut démarrer!");
+            int propositions=12;
+            System.out.println("Vous disposez de "+propositions+" propositions");
+        }
 
 
-    }
+
 }
